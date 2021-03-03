8fedf2cbe08653fd961d1831c3aae6452d401f6a
Pardhu Madipalli <pardhu1212@gmail.com>
2021-03-03 23:54:16 +0530

    Bump req version to 3.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="instagram-insights",
-    version="0.2.4",
+    version="0.2.6",
     author="Pardhu Madipalli",
     author_email="pardhu.madipalli@gmail.com",
     description="Get insights about best times to post, and the best tags based on likes, views and impressions.",
@@ -24,7 +24,7 @@ setuptools.setup(
     package_data={'insights': ['data/*']},
     data_files=[('.', ['screenshot.png', 'README.md'])],
     py_modules = ['constant', 'setup', 'get_insta_insights', 'commons'],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=['numpy', 'pandas'],
     entry_points={"console_scripts": ["insta-insights=get_insta_insights:main"]},
 )
