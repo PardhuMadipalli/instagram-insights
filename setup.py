eb7f27272cce19d0d4c4976a1bd8147ed7888c23
Pardhu Madipalli <pardhu1212@gmail.com>
2021-03-06 18:22:46 +0530

    Enable codecov for develop, Update version to 0.2.7  (#12)

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as req_file:
+    requirements = req_file.readlines()
+    requirements = [x.strip() for x in requirements]
+
 setuptools.setup(
     name="instagram-insights",
-    version="0.2.6",
+    version="0.2.7",
     author="Pardhu Madipalli",
     author_email="pardhu.madipalli@gmail.com",
     description="Get insights about best times to post, and the best tags based on likes, views and impressions.",
@@ -23,8 +27,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     package_data={'insights': ['data/*']},
     data_files=[('.', ['screenshot.png', 'README.md'])],
-    py_modules = ['constant', 'setup', 'get_insta_insights', 'commons'],
+    py_modules = ['constant', 'setup', 'get_insta_insights', 'commons', 'requirements.txt', 'requirements-test.txt'],
     python_requires=">=3.7",
-    install_requires=['numpy', 'pandas', 'requests', 'matplotlib'],
+    install_requires=requirements,
     entry_points={"console_scripts": ["insta-insights=get_insta_insights:main"]},
 )
